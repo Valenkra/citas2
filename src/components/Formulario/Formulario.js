43f7139c425cb1fd91369bfab0e061e19e633fe6
VALENTINA ORIT VUGIN <47866545@ort>
2024-05-15 09:05:54 -0300

    validaciones

--- src/components/Formulario/Formulario.js
+++ src/components/Formulario/Formulario.js
@@ -5,35 +5,44 @@ import './Formulario.css';
 function Formulario({ citas, setCita}) {
     
     const crearCita = (e) => {
-        if (window.confirm("Confirmas que queres agregar esta cita?")) {
-        e.preventDefault();
-        setCita([...citas, {
-            nMascota: e.target.nMascota.value,
-            nDuenio: e.target.nDuenio.value,
-            fecha: e.target.fecha.value,
-            hora: e.target.hora.value,
-            sintomas: e.target.sintomas.value
-        }]);
-        }else alert("La cita no ha sido creada");
+        let pre = e.target;
+        if ((pre.nMascota.value === null || pre.nMascota.value === "") && (pre.nDuenio.value === null || pre.nDuenio.value === "") && 
+        (pre.fecha.value === null || pre.fecha.value === "") && (pre.hora.value === null || pre.hora.value === "")) {
+            alert("Please Fill In All Required Fields");
+            return false;
+          }
+          else{
+
+            if (window.confirm("Confirmas que queres agregar esta cita?")) {
+                e.preventDefault();
+                setCita([...citas, {
+                    nMascota: e.target.nMascota.value,
+                    nDuenio: e.target.nDuenio.value,
+                    fecha: e.target.fecha.value,
+                    hora: e.target.hora.value,
+                    sintomas: e.target.sintomas.value
+                }]);
+                }else alert("La cita no ha sido creada");
+          }
 }
 
     return (
             <form onSubmit={crearCita}>
                 <div className="casillaForm">
                     <label>Nombre Mascota</label>
-                    <input type="text"  name="nMascota" placeholder="Nombre de tu mascota"/>
+                    <input type="text"  name="nMascota" placeholder="Nombre de tu mascota" required/>
                 </div>
                 <div className="casillaForm">
                     <label>Nombre Dueño</label>
-                    <input type="text"  name="nDuenio" placeholder="Nombre dueño de la mascota"/>
+                    <input type="text"  name="nDuenio" placeholder="Nombre dueño de la mascota" required/>
                 </div>
                 <div className="casillaForm">
                     <label>Fecha</label>
-                    <input type="date" name="fecha"/>
+                    <input type="date" name="fecha" required/>
                 </div>
                 <div className="casillaForm">
                     <label>Hora</label>
-                    <input type="time" name="hora"/>
+                    <input type="time" name="hora" required/>
                 </div>
                 <div className="casillaForm">
                     <label>Sintomas</label>
