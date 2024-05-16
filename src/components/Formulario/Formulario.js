4fe1484f968e9613dcc9bb01969fe8f59a1fd427
Valenkra <47866545@est.ort.edu.ar>
2024-05-16 08:23:02 -0300

    siguiendo

--- src/components/Formulario/Formulario.js
+++ src/components/Formulario/Formulario.js
@@ -6,13 +6,13 @@ function Formulario({ citas, setCita}) {
     
     const crearCita = (e) => {
         let pre = e.target;
-        if ((pre.nMascota.value === null || pre.nMascota.value === "") && (pre.nDuenio.value === null || pre.nDuenio.value === "") && 
-        (pre.fecha.value === null || pre.fecha.value === "") && (pre.hora.value === null || pre.hora.value === "")) {
+        if ((pre.nMascota.value === null || pre.nMascota.value.replace(" ", "").length <= 0) && (pre.nDuenio.value === null || pre.nDuenio.value.replace(" ", "").length <= 0) && 
+        (pre.fecha.value === null || pre.fecha.value.replace(" ", "").length <= 0) && (pre.hora.value === null || pre.hora.value.replace(" ", "").length <= 0)) {
             alert("Carga todos los campos necesarios");
             return false;
           }
           else{
-
+            console.log(pre.nDuenio.value === "");
             if (window.confirm("Confirmas que queres agregar esta cita?")) {
                 e.preventDefault();
                 setCita([...citas, {
@@ -30,19 +30,19 @@ function Formulario({ citas, setCita}) {
             <form onSubmit={crearCita}>
                 <div className="casillaForm">
                     <label>Nombre Mascota</label>
-                    <input type="text"  name="nMascota" placeholder="Nombre de tu mascota" required/>
+                    <input type="text"  name="nMascota" placeholder="Nombre de tu mascota"/>
                 </div>
                 <div className="casillaForm">
                     <label>Nombre Dueño</label>
-                    <input type="text"  name="nDuenio" placeholder="Nombre dueño de la mascota" required/>
+                    <input type="text"  name="nDuenio" placeholder="Nombre dueño de la mascota"/>
                 </div>
                 <div className="casillaForm">
                     <label>Fecha</label>
-                    <input type="date" name="fecha" required/>
+                    <input type="date" name="fecha"/>
                 </div>
                 <div className="casillaForm">
                     <label>Hora</label>
-                    <input type="time" name="hora" required/>
+                    <input type="time" name="hora"/>
                 </div>
                 <div className="casillaForm">
                     <label>Sintomas</label>
